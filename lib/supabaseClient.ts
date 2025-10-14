f3865ecffd4b93b367cbe954cd2adaedd543cc3f
alexandersimoes <alexandersimoes@gmail.com>
2025-10-14 19:49:41 -0400

    debugging for user logins

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -12,7 +12,7 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     persistSession: true,
     autoRefreshToken: true,
-    detectSessionInUrl: true,
+    detectSessionInUrl: false,
     storage: typeof window !== 'undefined' ? window.localStorage : undefined,
   }
 });
